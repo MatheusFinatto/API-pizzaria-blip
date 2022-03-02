80d32e7f3a120fcc33ce8c53b82341dbf822ee8b
Matheus Finatto <matheusfinatto2@gmail.com>
2022-03-02 16:13:29 -0300

    atualização de dados pizzas

--- index.js
+++ index.js
@@ -4,25 +4,27 @@ const app = express();
 const db = [
     {
      "nome": "Quatro queijos",
-     "valor": 50
+     "valor": 50,
+     "img": "https://www.comidaereceitas.com.br/img/sizeswp/1200x675/2007/12/Pizza_quatro_queijossss.jpg",
+     "slogan":"A mais vendida!"
     },
 
     {
      "nome":"Margherita",
-     "valor": 40
+     "valor": 40,
+     "img":"https://i0.wp.com/descobrindoasicilia.com/wp-content/uploads/2020/11/amirali-mirhashemian-zTxiTnTag78-unsplash.jpg?ssl=1",
+     "slogan":"Um clássico atemporal!"
     },
 
     {
      "nome":"Lombo com abacaxi",
-     "valor": 35 
+     "valor": 35,
+     "img":"https://receitinhas.s3-sa-east-1.amazonaws.com/wp-content/uploads/2017/06/iStock-504427632-848x477.jpeg",
+     "slogan":"Uma blasfêmia, mas há quem goste."      
     }
 ]
 
 app.use(express.json())
-
-app.get("/api/pizzas", (req,res) => {
-    res.send(db)
-})
 
 app.post("/api/pizzas", (req,res) =>{
     const dados = {
