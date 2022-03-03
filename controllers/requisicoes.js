893cc4a478041252de3323f37a63547ae017cfc7
MatheusFinatto <matheusfinatto2@gmail.com>
2022-03-03 11:55:20 -0300

    organizacao de arquivos

---
+++ controllers/requisicoes.js
@@ -0,0 +1,43 @@
+const db = require('../database/db')
+
+module.exports = app =>{
+    app.get("/api/pizzas/", (req,res) =>{
+        res.status(200).send(db)
+    })
+    
+    app.post("/api/pizzas", (req,res) =>{
+        const dados = {
+            nome: req.body.nome,
+            valor: req.body.valor,
+            img: req.body.img,
+            slogan: req.body.slogan
+        }
+        db.push(dados)
+        res.status(201).send(dados)
+    })
+    
+    app.get("/api/pizzas/:id", (req,res) =>{
+        let id =  req.params.id - 1
+        res.status(200).send(db[id])
+    })
+    
+    app.put("/api/pizzas/:id", (req,res) =>{
+        let id =  req.params.id - 1
+        const dados = {
+            nome: req.body.nome,
+            valor: req.body.valor,
+            img: req.body.img,
+            slogan: req.body.slogan
+
+        }
+        db[id] = dados
+        res.status(200).send(db[id])
+    })
+    
+    app.delete("/api/pizzas/:id", (req,res) =>{
+        let id =  req.params.id - 1
+        db.splice(id,1)
+        res.status(200).send("A pizza foi deletada com sucesso")
+    })
+}
+
---
+++ database/db.js
@@ -0,0 +1,25 @@
+
+const db = [
+    {
+     "nome": "Quatro queijos",
+     "valor": 50,
+     "img": "https://www.comidaereceitas.com.br/img/sizeswp/1200x675/2007/12/Pizza_quatro_queijossss.jpg",
+     "slogan":"A mais vendida!"
+    },
+
+    {
+     "nome":"Margherita",
+     "valor": 40,
+     "img":"https://i0.wp.com/descobrindoasicilia.com/wp-content/uploads/2020/11/amirali-mirhashemian-zTxiTnTag78-unsplash.jpg?ssl=1",
+     "slogan":"Um clássico atemporal!"
+    },
+
+    {
+     "nome":"Lombo com abacaxi",
+     "valor": 35,
+     "img":"https://receitinhas.s3-sa-east-1.amazonaws.com/wp-content/uploads/2017/06/iStock-504427632-848x477.jpeg",
+     "slogan":"Uma blasfêmia, mas há quem goste."      
+    }
+]
+
+module.exports = db;
